eb924a658fcccff9c2d338e49089f655d0fb2dc8
yui-0326 <maeda.yuiha.infratop@gmail.com>
2021-06-06 18:32:50 +0000

    create post function

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5b9cf7eb0e416970b5a5ecf01bb609f653def817c5a4d717d463595d620d4df0ec92a645d0ebb57f0e7f08b0ce9069ed2f42e11651b7afd135a4879ca76e2a56'
